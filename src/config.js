4d455459d7301ffbf7d8875b3f3ae8f41226c63e
dpatel38 <daminipatels@gmail.com>
2021-04-06 16:41:09 -0400

    create react-firebase chat app

---
+++ src/config.js
@@ -0,0 +1,10 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyAf_if-VqB4mEjc_tkgoqK0lhPpTD_wfb0",
+    authDomain: "react-firebase-chat-app-824d7.firebaseapp.com",
+    projectId: "react-firebase-chat-app-824d7",
+    storageBucket: "react-firebase-chat-app-824d7.appspot.com",
+    messagingSenderId: "852757755989",
+    appId: "1:852757755989:web:ac43e77d707a17aa283cdc"
+  };
+
+  export default firebaseConfig;
